94f16f4783292038f994873d034a657c1c5c1d19
dharmaseervi <dharmaseervijb18239@gmail.com>
2026-05-07 00:34:02 +0530

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,22 @@
-import Features from '@/components/home/Features'
-import VideoSection from '@/components/home/VideoSection'
+import Header from "@/components/home/Header"
 
-import AnimatedHearingAid from '@/components/home/HearingAnimation'
-import HearingTest from '@/components/home/HearingTest'
-import Header from '@/components/home/Header'
-import BentoGrid from '@/components/BentoGrid'
-import Footer from '@/components/home/Footer'
-
-
+import VideoSection from "@/components/home/VideoSection"
+import Footer from "@/components/home/Footer"
+import BentoGrid from "@/components/BentoGrid"
+import TechnologyShowcase from "@/components/home/HearingAnimation"
+import Features from "@/components/home/Features"
+import HearingTestSection from "@/components/home/HearingTest"
 
 export default function HomePage() {
   return (
-    <div className="bg-gradient-to-b from-gray-50 to-white">
+    <main>
       <Header />
-      <AnimatedHearingAid  />
       <BentoGrid />
       <Features />
-      <HearingTest />
+      <TechnologyShowcase />
+      <HearingTestSection />
       <VideoSection />
       <Footer />
-    </div>
+    </main>
   )
 }
-
